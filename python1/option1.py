6ea04f22a61db73442d5bca0b127983435f34ecd
Yuba32 <92969369+Yuba32@users.noreply.github.com>
2022-04-27 13:41:30 +0900

    opt1

---
+++ python1/option1.py
@@ -0,0 +1,21 @@
+
+def isprime(n):
+    flg = 0
+    for i in range(n):
+        if(n % 2 == 0):
+            print('Prime')
+            flg = 1
+            break
+        else:
+            continue
+    if(flg == 0):
+        print(n)
+
+
+def main():
+    for num in range(30,60):
+        isprime(num)
+
+
+if __name__ == "__main__":
+    main()
