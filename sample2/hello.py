8be7aaa2ae2764e6c424a40fd6249fef7bbf1608
Yuba32 <92969369+Yuba32@users.noreply.github.com>
2022-05-11 14:00:37 +0900

    [add] hello

---
+++ sample2/hello.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template('hello.html')
+
+
+@app.route("/<name>")
+def hello_user(name):
+    return render_template("hello.html",name=name)
